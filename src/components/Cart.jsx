7afe14f20c05c1c69c5d517ff98e548875ae8426
miki <milkyas.dev@gmail.com>
2026-03-06 03:58:14 -0300

    implemented fetch API and enhanced UI

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,45 @@
 function Cart() {
-  return <div>Cart Page</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-10">
+        <button className=" py-2 px-4 font-bold bg-blue-400 text-white rounded-md">
+          check out
+        </button>
+        <h1 className="py-4">Total: $200</h1>
+      </div>
+      <div className="flex items-center bg-white  overflow-hidden mb-4 p-4">
+        {/* Product Image */}
+        <img
+          src={"image"}
+          alt={"name"}
+          className="w-24 h-24 object-cover rounded-md bg-white border border-black/5 shadow-sm"
+        />
+
+        {/* Product Info */}
+        <div className="flex-1 ml-4">
+          <h2 className="text-lg font-semibold font-cinzel text-gray-800">
+            {"name"}
+          </h2>
+          <p className="text-gray-600 mt-1">$ {"price"}</p>
+          <p className="text-gray-500 mt-1">{"2"} PCS</p>
+        </div>
+
+        {/* Remove / Action Buttons */}
+        <div className="flex justify-between flex-col  gap-4">
+          <div className="flex justify-end gap-4">
+            <button className="bg-white  text-black border border-black/5 shadow-sm  text-sm  hover:shadow-lg  font-semibold py-1 px-2 rounded">
+              +
+            </button>
+            <button className="bg-white text-black  border border-black/5 shadow-sm  text-sm  hover:shadow-lg  font-semibold py-1 px-2 rounded">
+              -
+            </button>
+          </div>
+          <button className="bg-white text-black  border border-black/5 shadow-sm  text-sm  hover:shadow-lg  font-semibold  p-2 rounded">
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Cart;
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,34 +1,18 @@
+import { useEffect, useState } from "react";
+
 function ProductList() {
-  const products = [
-    {
-      id: 1,
-      name: "Stylish Shoes",
-      price: 79.99,
-      image:
-        "https://images.unsplash.com/photo-1606813904304-3b182c25c719?auto=format&fit=crop&w=500&q=80",
-    },
-    {
-      id: 2,
-      name: "Classic Watch",
-      price: 149.99,
-      image:
-        "https://images.unsplash.com/photo-1611078489740-5a2cd56f3b08?auto=format&fit=crop&w=500&q=80",
-    },
-    {
-      id: 3,
-      name: "Leather Bag",
-      price: 120.0,
-      image:
-        "https://images.unsplash.com/photo-1580910051070-09ed5ee10850?auto=format&fit=crop&w=500&q=80",
-    },
-    {
-      id: 4,
-      name: "Sunglasses",
-      price: 49.99,
-      image:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?auto=format&fit=crop&w=500&q=80",
-    },
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1 className="mb-10">Product List</h1>
@@ -37,7 +21,7 @@ function ProductList() {
           <ProductCard
             key={product.id}
             image={product.image}
-            name={product.name}
+            name={product.title}
             price={product.price}
           />
         ))}
@@ -47,16 +31,18 @@ function ProductList() {
 }
 const ProductCard = ({ image, name, price }) => {
   return (
-    <div className="w-[250px] rounded-lg overflow-hidden">
+    <div className=" bg-white border border-black/5 shadow-sm w-[250px] rounded-lg overflow-hidden py-4">
       <img
         src={image}
         alt={name}
         className="w-full h-48 object-contain text-center "
       />
       <div className="p-4 text-center">
-        <h2 className="text-xl font-bold font-cinzel text-gray-800">{name}</h2>
+        <h2 className="font-bold font-cinzel text-gray-800">
+          {name.length > 20 ? `${name.slice(0, 15)}...` : name}
+        </h2>
         <p className="mt-2 text-gray-600 text-lg">${price}</p>
-        <button className="mt-4  bg-blue-400 hover:bg-blue-300 text-white font-semibold py-2 px-4 rounded">
+        <button className="mt-4  border-black border text-black font-semibold py-2 px-4 rounded">
           Add to Cart
         </button>
       </div>
