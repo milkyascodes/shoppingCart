7eab243192ba2ab22400884d0b2cc660e84b8b3b
miki <milkyas.dev@gmail.com>
2026-03-07 06:45:42 -0300

    created authenticaton routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProductList from "./components/ProductList";
 import Cart from "./components/Cart";
 import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<ProductList />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<ProductList />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,42 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-  const { items: cartItems, tempItems } = useSelector((state) => state.cart);
+  const [isLoginPage, setIsLoginPage] = useState(false);
+  const { items: cartItems } = useSelector((state) => state.cart);
+
+  const isLoggedIn = false;
 
   return (
     <nav className="flex items-center justify-between w-full mx-auto max-w-lg mb-10">
       <h1 className="text-3xl font-bold">SHOPPERS</h1>
-      <div className="flex items-center gap-5">
-        <Link to="/">Home</Link>
+      {isLoggedIn ? (
+        <div className="flex items-center gap-5">
+          <Link to="/">Home</Link>
 
-        <Link to="/cart">
-          <div className="relative">
-            <button className="p-2  rounded"> 🛒</button>
+          <Link to="/cart">
+            <div className="relative">
+              <button className="p-2  rounded"> 🛒</button>
 
-            {cartItems.length !== 0 && (
-              <span className="absolute transition-all -top-2 -right-2 flex items-center justify-center w-5 h-5 text-xs text-white bg-red-500 rounded-full">
-                {cartItems.length}
-              </span>
-            )}
-          </div>
-        </Link>
-      </div>
+              {cartItems.length !== 0 && (
+                <span className="absolute transition-all -top-2 -right-2 flex items-center justify-center w-5 h-5 text-xs text-white bg-red-500 rounded-full">
+                  {cartItems.length}
+                </span>
+              )}
+            </div>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link
+            onClick={() => setIsLoginPage((state) => !state)}
+            to={!isLoginPage ? "/login" : "/"}
+          >
+            {isLoginPage ? "Create New Account" : "Login"}
+          </Link>
+        </div>
+      )}
     </nav>
   );
 }
