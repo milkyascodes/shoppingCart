41ea95e45f8fa9d0c1c9c0ad469773b5a109e2eb
miki <milkyas.dev@gmail.com>
2026-03-08 03:46:18 -0300

    ui enhancement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { logout } from "../features/authentication/authSlice";
@@ -6,10 +6,9 @@ import { logout } from "../features/authentication/authSlice";
 function Navbar() {
   const auth = useSelector((state) => state.auth);
   const { items: cartItems } = useSelector((state) => state.cart);
-
   const location = useLocation();
-
   const dispatch = useDispatch();
+
   function getLinkInfo(path) {
     switch (path) {
       case "/":
@@ -21,6 +20,46 @@ function Navbar() {
       default:
         return { to: "/register", text: "Create New Account" };
     }
+  }
+
+  function AvatarMenu({ user, logout }) {
+    const [open, setOpen] = useState(false);
+
+    return (
+      <div className="relative">
+        {/* Avatar */}
+        <button
+          onClick={() => setOpen(!open)}
+          className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-800 text-white font-bold"
+        >
+          {user.username?.charAt(0).toUpperCase()}
+        </button>
+
+        {/* Dropdown */}
+        {open && (
+          <div className="absolute right-0 mt-2 w-40 bg-white shadow-lg rounded-lg border">
+            <div className="px-4 py-2 border-b text-sm text-gray-600">
+              {user?.name}
+            </div>
+
+            {/* <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
+              Profile
+            </button>
+
+            <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
+              Orders
+            </button> */}
+
+            <button
+              onClick={logout}
+              className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100"
+            >
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
+    );
   }
 
   const { to, text } = getLinkInfo(location.pathname);
@@ -52,7 +91,15 @@ function Navbar() {
               )}
             </div>
           </Link>
-          <button
+          <AvatarMenu
+            user={auth.currentUser}
+            logout={() => {
+              dispatch(logout());
+              console.log("log", logout);
+            }}
+          />
+          {/* .... */}
+          {/* <button
             onClick={() => {
               dispatch(logout());
               console.log("log", logout);
@@ -60,7 +107,7 @@ function Navbar() {
             className=" font-bold"
           >
             Log out
-          </button>{" "}
+          </button>{" "} */}
         </div>
       )}
     </nav>
